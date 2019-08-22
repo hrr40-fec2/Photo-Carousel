eddd7a084ab9c6f1647c9dfc418fd28d1b8bdd11
Mr Myers <myersniiansah@gmail.com>
2019-08-22 16:44:12 -0400

    initial plan for initial dummy data, waiting for review

---
+++ database-mongodb/seed.js
@@ -0,0 +1,11 @@
+var Photos = require('./index.js');
+const axios = require('axios');
+
+//create a function that takes a number input
+  //create a loop that goes up to n
+    //create a get request for the movies n times
+    // then take the data and save it to the database
+    // also save the image urls to S3
+
+//add the seed script to package.json
+
