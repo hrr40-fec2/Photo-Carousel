d47b8ff68e4929f9b8f5040083b9c3e32e51047a
Mr Myers <myersniiansah@gmail.com>
2019-08-22 21:01:44 -0400

    refactored random image function

---
+++ database/index.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/images', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected to database');
+});
+
+var photoSchema = new mongoose.Schema({
+  title: String,
+  director: String,
+  cast: String,
+  mpaaRating,
+  summary: String,
+  releaseDate: String,
+  imdbRating,
+  runtime: String,
+  genre: [String],
+  imageUrls: [String]
+});
+
+var Photo = mongoose.model('Photo', photoSchema);
+
+module.exports = Photo;
