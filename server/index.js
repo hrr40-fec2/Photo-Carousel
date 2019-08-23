8a6ad45b12a8c2e520b7cab5a5117d0b8dee7e58
Mr Myers <myersniiansah@gmail.com>
2019-08-22 21:42:22 -0400

    added static S3 url

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var port = 3000;
 
 app.use(express.static('./client/dist'));
+app.use(express.static('http://hrr40-fec2-movie-list.s3-website.ca-central-1.amazonaws.com'));
 
 app.get('/api/photos', (req, res) => {
 });
