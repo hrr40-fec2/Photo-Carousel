2e307332849fdcc77e4f4cdacf948345ee0d879f
Mr Myers <myersniiansah@gmail.com>
2019-08-27 19:05:47 -0400

    stub out initial tests for Carousel

--- __tests__/Carousel.test.js
+++ __tests__/Carousel.test.js
@@ -3,7 +3,23 @@ import React from 'react';
 import Tests from '../client/src/setupTests.js';
 import Carousel from '../client/src/components/Carousel.jsx';
 
-//it should move from one slide to the next
-//it should hold 25 photos
-//renders a Carousel component
-//should have an alt image url
+describe('Carousel component', () => {
+  //it should move from one slide to the next
+  it('should be able to change slides' () => {
+
+  });
+  //it should hold 25 photos
+  it('should hold 25 photos', () => {
+
+  });
+
+  //renders a Carousel component
+  it('renders correctly', () => {
+
+  });
+
+  //should have an alt image url
+  it('should hold images', () => {
+
+  });
+});
