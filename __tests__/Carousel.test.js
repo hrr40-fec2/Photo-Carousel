d3dabceae28baffe3874ddaa635feccfc3d1b76e
Mr Myers <myersniiansah@gmail.com>
2019-08-27 18:31:02 -0400

    created test for carousel

---
+++ __tests__/Carousel.test.js
@@ -0,0 +1,9 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Tests from '../client/src/setupTests.js';
+import Carousel from '../client/src/components/Carousel.jsx';
+
+//it should move from one slide to the next
+//it should hold 25 photos
+//renders a Carousel component
+//should have an alt image url
