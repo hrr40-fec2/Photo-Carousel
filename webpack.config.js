0a6ea75947ef8e01c7c214a760025f184f83e5cf
Mr Myers <myersniiansah@gmail.com>
2019-08-27 21:59:39 -0400

    added config for react-slick css

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
             presets: ['@babel/preset-react', '@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+            {loader: "style-loader"},
+            {loader: "css-loader"}
+        ]
       }
     ]
   },
