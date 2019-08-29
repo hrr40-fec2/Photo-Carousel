3923be9ddb70b5882e84608f8f833cb10f73777f
Mr Myers <myersniiansah@gmail.com>
2019-08-29 18:34:11 -0400

    added more tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -14,6 +14,7 @@ describe('GET request for images', () => {
   test('returns an array of images', async () => {
     const response = await request(app).get('http://localhost:3000/api/allPhotos')
     expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBe(12);
   });
 });
 
@@ -21,5 +22,6 @@ describe('GET request for movies', () => {
   test('returns an array of movies', async () => {
     const response = await request(app).get('http://localhost:3000/api/photos')
     expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBe(12);
   });
 });
