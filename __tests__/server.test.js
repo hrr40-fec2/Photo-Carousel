2aeec060acefdd940181939a43509564c60877bf
Mr Myers <myersniiansah@gmail.com>
2019-08-29 15:57:51 -0400

    switched to async await style

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,2 +1,25 @@
+/**
+ * @jest-environment node
+ */
+
 var request = require('supertest');
 var app = require('../server/index.js');
+
+describe('GET request for images', () => {
+  test('should response the GET method', async () => {
+    const response = await request(app).get('http://localhost:3000/api/allPhotos')
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('returns an array of images', async () => {
+    const response = await request(app).get('http://localhost:3000/api/allPhotos')
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
+
+describe('GET request for movies', () => {
+  test('returns an array of movies', async () => {
+    const response = await request(app).get('http://localhost:3000/api/photos')
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
