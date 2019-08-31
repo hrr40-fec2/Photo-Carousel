1ef14f93a5e3706f959184ee72dc34cfa763b6f8
Mr Myers <myersniiansah@gmail.com>
2019-08-31 11:42:41 -0400

    reformatted test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -7,21 +7,21 @@ var app = require('../server/index.js');
 
 describe('GET request for images', () => {
   test('should response the GET method', async () => {
-    const response = await request(app).get('http://localhost:3000/api/allPhotos')
+    const response = await request(app).get('/api/allPhotos')
     expect(response.statusCode).toBe(200);
   });
 
   test('returns an array of images', async () => {
-    const response = await request(app).get('http://localhost:3000/api/allPhotos')
+    const response = await request(app).get('/api/allPhotos')
     expect(Array.isArray(response.body)).toBe(true);
-    expect(response.body.length).toBe(12);
+    expect(response.body.length).toBe(25);
   });
 });
 
 describe('GET request for movies', () => {
   test('returns an array of movies', async () => {
-    const response = await request(app).get('http://localhost:3000/api/photos')
+    const response = await request(app).get('/api/photos')
     expect(Array.isArray(response.body)).toBe(true);
-    expect(response.body.length).toBe(12);
+    expect(response.body.length).toBe(10);
   });
 });
