d1fdbba57c54f4d43436d07c3e38b7245a1c1981
Mr Myers <myersniiansah@gmail.com>
2019-09-03 11:42:54 -0400

    configured setup to be able to test react-slick

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,4 +3,18 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({  adapter: new Adapter() });
 
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+    };
+  };
 
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  function(callback) {
+    setTimeout(callback, 0);
+  };
